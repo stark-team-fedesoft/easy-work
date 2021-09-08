3d18b62a4e7e3f933eaf806c35ff09688f096c79
Juan Felipe Ramirez Sanchez <juanfeliperamirezsanchez@gmail.com>
2021-09-08 08:25:13 -0500

    Se termina el usuario y el rol

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express        = require('express');
 const cors           = require('cors');
 const {dbconnection} = require('./db/db');
+// rutas
+const taskRoutes = require('./routes/tasks.route');
 const Role = require("./routes/role");
 const User = require("./routes/user");
 
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/tasks', taskRoutes);
 app.use("/api/role", Role);
 app.use("/api/user", User);
 
