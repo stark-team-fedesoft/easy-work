99d43e9f23b352baa9c99755cf4d8b1421b9f82c
Cristhian Zambrano <cz4785464@gmail.com>
2021-09-08 09:08:21 -0500

    modified index.js

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,12 @@ const express        = require('express');
 const cors           = require('cors');
 const {dbconnection} = require('./db/db');
 // rutas
-<<<<<<< HEAD
 const taskRoutes     = require('./routes/tasks.route');
 const taskListRoutes = require('./routes/tasks-list.route');
-=======
-const taskRoutes = require('./routes/tasks.route');
 const Role = require("./routes/role");
 const User = require("./routes/user");
 
 
->>>>>>> felipe
 
 require("dotenv").config();
 
@@ -21,14 +17,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/tasks', taskRoutes);
-<<<<<<< HEAD
 app.use('/api/tasks-list', taskListRoutes);
-=======
 app.use("/api/role", Role);
 app.use("/api/user", User);
 
 
->>>>>>> felipe
 
 app.listen( process.env.PORT, () =>
     console.log("Backend server running on port: " + process.env.PORT )
