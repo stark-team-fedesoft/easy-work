5bd5151145137bc4d6899f6ab10a0182a00858f4
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-09-15 18:21:30 -0500

    merged tree

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,13 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-<<<<<<< HEAD
   production : false,
   BASE_URL   : 'http://localhost:3001/api',
-=======
-  production: false,
-  apiURL: 'http://localhost:3001/api/'
->>>>>>> sebastian
 };
 
 /*
